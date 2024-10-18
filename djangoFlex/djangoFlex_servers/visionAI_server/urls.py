a0db7461776286dd8a3d534e78a8df5dd895586c
Marcus <cxldun@gmail.com>
2024-10-18 17:58:35 +0800

    remove unuse

--- djangoFlex/djangoFlex_servers/visionAI_server/urls.py
+++ djangoFlex/djangoFlex_servers/visionAI_server/urls.py
@@ -4,8 +4,8 @@ from .api_object import ObjectDetectView
 from .api_db import VisionAIDBAPI
 from .api_draw import DrawView
 urlpatterns = [
-    path('violations_detect_service/', ViolationDetectView.as_view(), name='violation-detect'),
-    path('object_detect_service/', ObjectDetectView.as_view(), name='object-detect'),
-    path('vision_ai_db_service/', VisionAIDBAPI.as_view(), name='vision-ai-db'),
+    # path('violations_detect_service/', ViolationDetectView.as_view(), name='violation-detect'),
+    # path('object_detect_service/', ObjectDetectView.as_view(), name='object-detect'),
+    # path('vision_ai_db_service/', VisionAIDBAPI.as_view(), name='vision-ai-db'),
     path('draw_service/', DrawView.as_view(), name='draw-service'),
 ]
